1cbd9c23c7dfacae352c53c66cb61bcde51e4ef8
dellacrox <santiago5personal@gmail.com>
2022-06-16 07:51:37 -0700

    hola

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char *str = "\n";
+while (*str != '\0')
+_putchar(*str++);
+return (0);
+}
