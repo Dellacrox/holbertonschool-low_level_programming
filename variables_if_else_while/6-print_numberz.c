b6495d1bc0c9d51130c75f849b463b29bec94e58
dellacrox <santiago5personal@gmail.com>
2022-06-15 17:59:20 -0700

    hola

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,14 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)  
+int main(void)
 {
-  
-int c;  
+
+int c;
 for (c = '0'; c <= '9'; c++)
-{      
-putchar(c);      
-}  
-putchar('\n');  
-return (0); 
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
